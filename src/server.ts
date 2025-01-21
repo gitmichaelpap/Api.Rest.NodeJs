78e7bedfcc68bd89fc012ea8fa03815be72541e3
Michael Pacheco <michaelpacheco.br@gmail.com>
2025-01-21 20:43:51 -0300

    class 3

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.get("/", async (request, reply) => {
-  return "world";
-});
+app.get('/', async () => {
+  return 'world'
+})
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("Server started at http://localhost:3333");
-});
+  console.log('Server started at http://localhost:3333')
+})
