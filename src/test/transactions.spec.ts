6b7cd9c4ff29e064193816c78258789275d91d37
Michael Pacheco <michaelpacheco.br@gmail.com>
2025-01-22 01:19:59 -0300

    class 14

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, describe, afterAll, it } from 'vitest'
+import { beforeAll, describe, afterAll, it, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../app'
 
@@ -21,6 +21,34 @@ describe('Transactions routes', () => {
                 type: 'credit',
             })
             .expect(201)
+    })
+
+    it('should be able to list all transactions', async () => {
+        const createTransactionResponse = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'Salario',
+                amount: 10000,
+                type: 'credit',
+            })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        if (!cookies) {
+            throw new Error('No cookies found')
+        }
+
+        const listTransactionResponse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200)
+
+        expect(listTransactionResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'Salario',
+                amount: 10000,
+            })
+        ])
     })
 })
 
