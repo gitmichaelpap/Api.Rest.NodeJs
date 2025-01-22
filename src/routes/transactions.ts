fc48abcdd9d065b18a09a726c098c6e89fd1e7c6
Michael Pacheco <michaelpacheco.br@gmail.com>
2025-01-21 23:41:59 -0300

    class 9

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,31 @@
 import { FastifyInstance } from "fastify"
+import { z } from "zod"
 import { knex } from "../database"
+import { randomUUID } from "crypto"
 
 export function transactionsRoutes(app: FastifyInstance) {
-    app.get("/", async () => {
-        const transactions = await knex("transactions").where("amount", 100).select("*")
 
+    app.get("/", async (req, res) => {
+        const transactions = await knex("transactions").select("*")
         return transactions
     })
+
+    app.post("/", async (req, res) => {
+        const createTransactionSchema = z.object({
+            title: z.string().nonempty(),
+            amount: z.number(),
+            type: z.enum(["credit", "debit"]),
+        })
+
+        const { title, amount, type } = createTransactionSchema.parse(req.body)
+
+        await knex("transactions").insert({
+            id: randomUUID(),
+            title,
+            amount: type === "debit" ? amount * -1 : amount,
+        })
+
+        return res.status(201).send()
+    })
+
 }
