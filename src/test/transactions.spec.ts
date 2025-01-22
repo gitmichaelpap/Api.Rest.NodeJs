da1531aba3bc388fa1cb24b011221190cb806541
Michael Pacheco <michaelpacheco.br@gmail.com>
2025-01-22 01:10:59 -0300

    class 13

---
+++ src/test/transactions.spec.ts
@@ -0,0 +1,26 @@
+import { test, beforeAll, describe, afterAll, it } from 'vitest'
+import request from 'supertest'
+import { app } from '../app'
+
+describe('Transactions routes', () => {
+
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to create a new transaction', async () => {
+        await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'Salario',
+                amount: 10000,
+                type: 'credit',
+            })
+            .expect(201)
+    })
+})
+
